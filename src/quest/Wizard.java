e9333e026d8573f93b1100f56e6762f004efb615
Leon Zuev <loonatune@gmail.com>
2023-03-20 23:22:47 +0200

    Fix Wizard

--- src/quest/Wizard.java
+++ src/quest/Wizard.java
@@ -1,9 +1,17 @@
 package quest;
 
-// что-то,что умеет волшебство
+// интерфейс "Волшебник" - что-то, что умеет творить волшебство
 public interface Wizard {
 
-  public abstract int getMana();
-  public abstract void tryCharm(String charmName);// попытка юзать заклинание
-  public abstract void levelUp(); // поднять уровень волшебной силы
+  int mana = 0; // так нельзя - здесь будет константа
+
+  default int getMana() { // получить количество оставшейся маны
+    return mana;
+  }
+
+  void tryCharm(String charmName); // попытка использовать заклинание
+
+  default void levelUp() { // поднять уровень волшебной силы
+    System.out.println("Поднять уровень волшебной силы");
+  }
 }
