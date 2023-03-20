1491013f06ed439e52bc23d392fdf6e72c4cc180
Leon Zuev <loonatune@gmail.com>
2023-03-20 22:41:27 +0200

    Finish CommunicationsRunner

--- src/communications/CommunicationsRunner.java
+++ src/communications/CommunicationsRunner.java
@@ -3,7 +3,33 @@ package communications;
 public class CommunicationsRunner {
 
   public static void main(String[] args) {
-    Pager testPager = new Pager();
-    testPager.sendMessage("test message", "somebody");
+    // new Phone() напистаь нельзя, Phone - это интерфейс
+    // new Messenger() написать нельзя, Messenger - это интерфейс
+    Pager testPager = new Pager("Тестовый исходный адрес");
+    testPager.sendMessage("Тестовое сообщение", "кому-то");
+
+    // при объявлении типа переменных можно использовать интерфейсы, как и обстрактные классы.
+    // Java про эту переменную будет знать только то,что описано в её типе:
+    // в случае с интерфейсом - только про методы из этого интерфейса
+    RotaryPhone testPhone = new RotaryPhone();
+    testPhone.call("112");
+    // если Phone testPhone, то так нельзя - интерфейс Phone ничего не знает про метод `endCall()`:
+    // testPhone.endCall();
+    Smartphone iPhone = new Smartphone();
+
+    // звоним маме со всех разновидностей телефонов
+    callMom(testPhone);
+    callMom(iPhone);
+
+    // отправляем сообщение папе со всех разновидностей "Отправителя сообщений"
+    textDad(testPager);
+    textDad(iPhone);
+  }
+  public static void callMom(Phone phone) {
+    phone.call("маме");
+  }
+
+  public static void textDad(Messenger messenger) {
+    messenger.sendMessage("Привет", "папе");
   }
 }
